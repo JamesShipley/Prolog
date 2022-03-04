24f9d907f225a0cc9ac8d16f47a5600a48ad65f0
James Shipley <jds119@texel03.doc.ic.ac.uk>
2022-03-04 11:46:12 +0000

    DIF

--- efficient_searches.pl
+++ efficient_searches.pl
@@ -24,19 +24,14 @@ dldfs(Paths, Depth, Solution):-
 	dldfs(FinalPath, Depth, Solution).
 
 
-ids(Paths, 1, Max, Solution):-
-  choose(Paths, Node),
-  goal(Node),
-  length(Paths,Length),
-  Length - 1 =< Max,
-  reverse(Paths, Solution).
+ids(Paths,CurDepth,MaxDepth,X):-dldfs(Paths,CurDepth,X).
+ids(Paths,CurDepth,MaxDepth,X):-CurDepth < MaxDepth, ids(Paths,CurDepth+1,MaxDepth,X).
 
-ids(Paths, 1, Max, Solution):-
-  expands(Paths, NewNode),
-	\+ member(NewNode, Paths),
-  combine(Paths, NewNode, FinalPath),
-	ids(FinalPath, 1,, Solution).
-
+idsh(Paths,Depth,[],X):- dldfs(Paths,Depth,Y),idsh(Paths,Depth+1,[Y],X).
+idsh(Paths,Depth,[Y | H],X):- dldfs(Paths,Depth,Y), any([Y|H],X).
+idsh(Paths,Depth,[Y | H],X):-dldfs(Paths,Depth,Z), Z=\=Y,idsh(Paths,Depth,[Z,Y|H],X).
+any([A|R],A).
+any([A|R],X):-any(R,X).
 
 
 expands([Node|_], NewNode):-
@@ -47,3 +42,6 @@ choose(Paths, Node):-
 
 combine(CurrentPaths, NewNode, Paths):-
   append([NewNode],CurrentPaths,Paths).
+
+nth0(0,[L|List],Elem):- L==Elem.
+nth0(Index,[L|List],Elem):-Index>0,Sub is Index-1, nth0(Sub,List,Elem).
